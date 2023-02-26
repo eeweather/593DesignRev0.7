37eb9cd120c96a925d285c20ce472776b62bdd45
Victoria Van Gaasbeck <vvan@mo.ece.pdx.edu>
2023-02-25 16:06:17 -0800

    uvm dpi-c folder

---
+++ UVM_TinyALU/tb_classes/predictor.c
@@ -0,0 +1,34 @@
+/*  ECE593 Project 2023wi
+*   Victoria Van Gaasbeck <vvan@pdx.edu>
+*   Julia Filipchuk <bfilipc2@pdx.edu>
+*   Emily Weatherford <ew22@pdx.edu>
+*   Daniel Keller <dk27@pdx.edu>
+*  
+*  C predictor function for TinyALU scoreboard
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "svdpi.h"
+
+
+ short predictor (svBitVecVal a, svBitVecVal b, svBitVecVal* op_set){
+    short result = 1;
+   
+   switch (*op_set){
+    printf("Op set in C is %d", *op_set);
+        case  1 :
+            result = a + b; 
+            break;
+        case 2: 
+            result = a & b;
+             break;
+        case 3: 
+            result = a ^ b; 
+            break;
+        case 4:
+            result = a * b; 
+            break;
+    }
+
+    return result;
+}
