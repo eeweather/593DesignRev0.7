9d0e4598bcac8f08e8cb8b71d037b618de7b3b17
Victoria Van Gaasbeck <vvan@auto.ece.pdx.edu>
2023-03-04 16:04:49 -0800

    updated basic sequences

--- tb/sequence_base.svh
+++ tb/sequence_base.svh
@@ -15,6 +15,8 @@ class sequence_base extends uvm_sequence #(item_base);
 	endfunction
 
 	agent_config agent_cfg;
+	
+	item_base tx;	
 
 	task init_start(input uvm_sequencer #(item_base) sqr, input agent_config agent_cfg);
 		this.agent_cfg = agent_cfg;
@@ -23,12 +25,13 @@ class sequence_base extends uvm_sequence #(item_base);
 
 	//the body task is where transactions (or sequence items, or whatever) are created, randomized, and started/finished
 	task body();
-		//TODO: one random transaction, will add repeat/loop for more random tests later
-		item_base tx;	
-		tx = item_base::type_id::create("tx");
-		start_item(tx);
-		if(!tx.randomize()) `uvm_fatal(get_type_name(), "tx.randomize failed")
-		finish_item(tx);
+	
+		repeat (10) begin
+			tx = item_base::type_id::create("tx");
+			start_item(tx);
+			if(!tx.randomize()) `uvm_fatal(get_type_name(), "tx.randomize failed")
+			finish_item(tx);
+		end
 	endtask: body
 
 endclass: sequence_base
