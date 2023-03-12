4f6082ec99167d75b23b5957345e639807547c95
Victoria Van Gaasbeck <vvan@auto.ece.pdx.edu>
2023-03-12 15:25:28 -0700

    new tasks in sequence base

--- tb/sequence_base.svh
+++ tb/sequence_base.svh
@@ -20,7 +20,6 @@ class sequence_base extends uvm_sequence #(item_base);
 	item_store store;
 
 
-
 	task init_start(input uvm_sequencer #(item_base) sqr);
 		this.start(sqr);
 	endtask: init_start
@@ -29,32 +28,32 @@ class sequence_base extends uvm_sequence #(item_base);
 	task body();
 	
 		//with default args returns a random transaction. 
-		repeat(2) get_alu_op();
+		//repeat(2) get_alu_op();
 
-		//repeat(2) daniel_sucks();
+		daniel_sucks();
 	endtask : body
 
-	// task daniel_sucks();
-	// 	tx = item_base::type_id::create("tx");
-	// 	alu = item_alu::type_id::create("alu");
-	// 	load = item_load::type_id::create("load");
-	// 	store = item_store::type_id::create("store");
-	// 	repeat(2) begin
-	// 		if(!load.randomize()) `uvm_fatal(get_type_name(), "load.randomize failed")
-	// 		start_item(load);
-	// 		finish_item(load);
-	// 	end
-	// 	if(!alu.randomize()) `uvm_fatal(get_type_name(), "alu.randomize failed")
-	// 	start_item(alu);
-	// 	finish_item(alu);
+	task daniel_sucks();
+		tx = item_base::type_id::create("tx");
+		alu = item_alu::type_id::create("alu");
+		load = item_load::type_id::create("load");
+		store = item_store::type_id::create("store");
+		repeat(2) begin
+			if(!load.randomize()) `uvm_fatal(get_type_name(), "load.randomize failed")
+			start_item(load);
+			finish_item(load);
+		end
+		if(!alu.randomize()) `uvm_fatal(get_type_name(), "alu.randomize failed")
+		start_item(alu);
+		finish_item(alu);
 
-	// 	if(!store.randomize()) `uvm_fatal(get_type_name(), "store.randomize failed")
-	// 	start_item(store);
-	// 	finish_item(store);
+		if(!store.randomize()) `uvm_fatal(get_type_name(), "store.randomize failed")
+		start_item(store);
+		finish_item(store);
 
-	// endtask : daniel_sucks
+	endtask : daniel_sucks
 
-
+	/**********************NEW TASKS*******************************************************/
 	 task load_data();
 		 load = item_load::type_id::create("load");
 		repeat(2) begin
